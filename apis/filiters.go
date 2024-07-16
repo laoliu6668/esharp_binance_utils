26d08b0482ee3e2eef0ad6a0dcf96b2a7162521d
lao liu <laoliu@gmail.com>
2024-07-16 10:42:50 +0800

    filter

--- apis/filiters.go
+++ apis/filiters.go
@@ -11,13 +11,22 @@ import (
 
 const gateway_vision = "data-api.binance.vision"
 
+type FilterSymbol struct {
+	BaseAssetPrecision int              `json:"baseAssetPrecision"` // 交易数量精度
+	QuotePrecision     int              `json:"quotePrecision"`     // 交易金额精度
+	Status             string           `json:"status"`             // TRADING
+	QuoteAsset         string           `json:"quoteAsset"`         // USDT
+	BaseAsset          string           `json:"baseAsset"`          // BTC
+	ContractType       string           `json:"contractType"`       // PERPETUAL(only-sawp)
+	Filters            []map[string]any `json:"filters"`
+}
 type ApiResponseFiliter struct {
-	Data []map[string]any `json:"symbols"`
+	Data []FilterSymbol `json:"symbols"`
 }
 
 // 获取全局过滤器
 // https://developers.binance.com/docs/zh-CN/binance-spot-api-docs/filters
-func GetFiliters() (data []map[string]any, err error) {
+func GetFiliters() (data []FilterSymbol, err error) {
 	const flag = "binance GetFiliters"
 	body, _, err := root.ApiConfig.Request("GET", gateway_vision, "/api/v3/exchangeInfo", nil, 0, false)
 	if err != nil {
@@ -36,9 +45,9 @@ func GetFiliters() (data []map[string]any, err error) {
 		return
 	}
 	// 过滤
-	data = []map[string]any{}
+	data = []FilterSymbol{}
 	for _, v := range res.Data {
-		if v["quoteAsset"] == "USDT" && v["status"] == "TRADING" {
+		if v.QuoteAsset == "USDT" && v.Status == "TRADING" {
 			data = append(data, v)
 		}
 	}
@@ -47,7 +56,7 @@ func GetFiliters() (data []map[string]any, err error) {
 
 // 获取期货交易对配置
 // https://developers.binance.com/docs/zh-CN/binance-spot-api-docs/filters
-func GetSwapSymbols() (data []map[string]any, err error) {
+func GetSwapSymbols() (data []FilterSymbol, err error) {
 	const flag = "binance GetSwapSymbols"
 	body, _, err := root.ApiConfig.Request("GET", gateway_fapi, "/fapi/v1/exchangeInfo", nil, 0, false)
 	if err != nil {
@@ -66,9 +75,9 @@ func GetSwapSymbols() (data []map[string]any, err error) {
 		return
 	}
 	// 过滤
-	data = []map[string]any{}
+	data = []FilterSymbol{}
 	for _, v := range res.Data {
-		if v["quoteAsset"] == "USDT" && v["status"] == "TRADING" {
+		if v.QuoteAsset == "USDT" && v.Status == "TRADING" && v.ContractType == "PERPETUAL" {
 			data = append(data, v)
 		}
 	}
